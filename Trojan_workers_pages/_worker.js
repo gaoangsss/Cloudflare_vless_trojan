3b443d59af5f6aeb4ec91f1a174c8c0496a6c3c4
gaoangsss <35525692+gaoangsss@users.noreply.github.com>
2024-11-17 22:32:24 +0800

    Update _worker.js

--- Trojan_workers_pages/_worker.js
+++ Trojan_workers_pages/_worker.js
@@ -1,7 +1,7 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
  
-let Pswd = "trojan";
+let Pswd = "bc71f0d9-401f-416c-9017-cb3916082994";
 const proxyIPs = ["ts.hpc.tw"]; //ts.hpc.tw edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 let cn_hostnames = [''];
 let CDNIP = 'www.visa.com.sg'
